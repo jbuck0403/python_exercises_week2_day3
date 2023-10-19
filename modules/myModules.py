fae84ef068daa19b0dccd9a9f9a0c644472d96e3
Jack Buck <jbuck0403@gmail.com>
2023-10-18 17:53:46 -0700

    first commit

---
+++ modules/myModules.py
@@ -0,0 +1,12 @@
+from math import pi
+
+def add(a, b):
+    return a + b
+
+def calcSqft(length, width):
+    """Takes 2 integers, length and width"""
+    return length * width
+
+def calcCircumference(radius):
+    """Takes one float, radius"""
+    return 2 * pi * radius
